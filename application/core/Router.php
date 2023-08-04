d70732c6a494e5e5c16923bf429204dde5312692
Altynbek312 <altyn_312@mail.ru>
2023-08-04 14:53:20 +0600

    TASK-2 (Project structure)
    feat: Init Router and Db classes
    
    Changed files:
    new file:   application/core/Router.php
    new file:   application/lib/Db.php
    new file:   application/lib/Dev.php
    new file:   index.php

---
+++ application/core/Router.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace application\core;
+
+class Router {
+    function __construct() {
+        echo "I'm Router <br>";
+}
+}
---
+++ application/lib/Db.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace application\lib;
+
+class Db {
+    function __construct() {
+        echo "I'm Db <br>";
+    }
+}
